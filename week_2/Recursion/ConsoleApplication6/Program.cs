2cb427e91ff3dabede50004cf0610625de25054d
shaihitdin@gmail.com <shaihitdin@gmail.com>
2017-01-27 00:55:49 +0600

    Second stage is done

---
+++ week_2/Recursion/ConsoleApplication6/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void emptySpace(int level)
+        {
+            for (int i = 0; i < level; ++i)
+                Console.Write("-");
+            Console.Write("> ");
+        }
+        static void Rec(string path, int level)
+        {
+            if (level > 3)
+                return;
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+            foreach (FileInfo file in files)
+            {
+                emptySpace(level);
+                Console.WriteLine(file.Name);
+            }
+            foreach (DirectoryInfo dInfo in directories)
+            {
+                emptySpace(level);
+                Console.WriteLine(dInfo.Name);
+                Rec(dInfo.FullName, level + 1);
+            }
+
+        }
+        static int N = 100000;
+        static string[] directory_stack = new string[N];
+        static int[] child_num = new int[N];
+        static int[] rec_level = new int[N];
+        static int sz;
+        static void add (string path, int stopnum, int lvl)
+        {
+            directory_stack[sz] = path;
+            child_num[sz] = stopnum;
+            rec_level[sz] = lvl;
+            sz++;
+        }
+        static void Main(string[] args)
+        {
+            //Rec(@"C:\Users\shaih_000\Documents\GitHub\CP\Olymp", 0);
+            add(@"C:\Users\shaih_000\Documents\GitHub\CP\Olymp", 0, 0);
+            while (sz > 0)
+            {
+                --sz;
+                string path = directory_stack[sz];
+                int child = child_num[sz];
+                int level = rec_level[sz];
+                DirectoryInfo directory = new DirectoryInfo(path);
+                FileInfo[] files = directory.GetFiles();
+                DirectoryInfo[] directories = directory.GetDirectories();
+                if (level > 3)
+                    continue;
+
+                if (child == 0)
+                {
+                    foreach (FileInfo file in files)
+                    {
+                        emptySpace(level);
+                        Console.WriteLine(file.Name);
+                    }
+                }
+                if (child >= directories.Length)
+                    continue;
+                DirectoryInfo dInfo = directories[child];
+                add(path, child + 1, level);
+                add(dInfo.FullName, 0, level + 1);
+                emptySpace(level);
+                Console.WriteLine(dInfo.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week_2/Recursion/ConsoleApplication6/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleApplication6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication6")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7618340a-6538-4bd6-8489-63789a04f5c2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
