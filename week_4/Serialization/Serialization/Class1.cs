9de0178e0206effb83fefa8055559ec11128971c
shaihitdin@gmail.com <shaihitdin@gmail.com>
2017-02-10 02:56:36 +0600

    Serialization in hand with Deserialization

---
+++ week_4/Serialization/Serialization/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class Complex
+    {
+        public int x, y;
+
+        static public int gcd(int x, int y)
+        {
+            if (y == 0)
+                return x;
+            return gcd(y, x % y);
+        }
+        public Complex(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return this.x + "/" + this.y;
+        }
+        static public Complex Simplify(Complex a)
+        {
+            int g = gcd(a.x, a.y);
+            return new Complex(a.x / g, a.y / g);
+        }
+
+        static public Complex operator +(Complex a, Complex b)
+        {
+            return Simplify(new Complex(a.x * b.y + b.x * a.y, a.y * b.y));
+        }
+
+    }
+}
---
+++ week_4/Serialization/Serialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream f = new FileStream(@"C:/work/hellou.privet", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter gut = new BinaryFormatter();
+            Complex a = new Complex(5, 3), b = new Complex(2, 2);
+            Complex c = new Complex(1, 1);
+            gut.Serialize(f, c);
+            c = a + b;
+            Console.WriteLine(c);
+            gut = new BinaryFormatter();
+            f.Close();
+            f = new FileStream(@"C:/work/hellou.privet", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            
+            c = gut.Deserialize(f) as Complex;
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
